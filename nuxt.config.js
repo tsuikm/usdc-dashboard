5f245fd71ca69f073dcf0eb0e382dd2e1eaf8714
Terrance Li <tyli@berkeley.edu>
2020-09-22 22:37:49 -0700

    Fix Material Icons not showing up

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,5 +2,17 @@ module.exports = {
   srcDir: 'src/',
   plugins: [
     '@/plugins/material.js'
-  ]
+  ],
+  head: {
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Roboto:400,500,700,400italic'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/icon?family=Material+Icons'
+      }
+    ]
+  }
 }

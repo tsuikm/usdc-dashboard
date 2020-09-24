a31b649108c96f111bddf2b762c98ceabe84667f
unknown <tetsuroescudero@berkeley.edu>
2020-09-23 22:32:56 -0700

    Can now route to /address, plus console logs for tests

---
+++ src/pages/address/_address.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Hello world</h1>
+</template>
+<script>
+export default {
+    mounted() {
+        console.log(this.$route.fullPath);
+        console.log(this.$route.params)
+      }
+    };
+</script>

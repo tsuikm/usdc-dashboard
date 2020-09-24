64d1631b50274ff5d1e5e11bda095dbf8966f1b7
Kelly Tsai <kellytsai@berkeley.edu>
2020-09-24 15:14:32 +0800

    created balance table file

---
+++ src/components/BalanceTable.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <md-table>
+      <md-table-row>
+        <md-table-head>Wallet Balance (USDC)</md-table-head>
+        <md-table-head>Conversion Rate</md-table-head>
+        <md-table-head>Wallet Balance (USD)</md-table-head>
+      </md-table-row>
+    </md-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BalanceTable",
+  components: {},
+  methods: {}
+};
+</script>
+
+<style scoped></style>

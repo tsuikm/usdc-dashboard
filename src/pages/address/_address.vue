639e8247135d3a729cffea7c7dbee29f8d5d0f29
unknown <tetsuroescudero@berkeley.edu>
2020-09-23 22:35:32 -0700

    Formatting

--- src/pages/address/_address.vue
+++ src/pages/address/_address.vue
@@ -1,11 +1,11 @@
 <template>
-    <h1>Hello world</h1>
+  <h1>Hello world</h1>
 </template>
 <script>
 export default {
-    mounted() {
-        console.log(this.$route.fullPath);
-        console.log(this.$route.params)
-      }
-    };
+  mounted() {
+    console.log(this.$route.fullPath);
+    console.log(this.$route.params);
+  },
+};
 </script>

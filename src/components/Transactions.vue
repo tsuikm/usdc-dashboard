d513ed1679012bb2e200d3bae8caa4d183776223
Constance Shi <constance.shi@berkeley.edu>
2020-09-25 19:17:41 -0700

    generate transactions for table display

--- src/components/Transactions.vue
+++ src/components/Transactions.vue
@@ -1,5 +1,26 @@
 <template>
-  <p @click="method">test</p>
+  <div>
+    <md-table md-card>
+      <md-table-toolbar>
+        <h1 class="md-title">Transactions</h1>
+      </md-table-toolbar>
+
+      <md-table-row>
+        <md-table-head md-numeric>ID</md-table-head>
+        <md-table-head>Name</md-table-head>
+        <md-table-head>Email</md-table-head>
+        <md-table-head>Gender</md-table-head>
+        <md-table-head>Job Title</md-table-head>
+      </md-table-row>
+
+      <md-table-row v-for="transaction in this.transactions" :key="transaction.transactionHash">
+        <md-table-cell>{{ transaction.transactionHash }}</md-table-cell>        
+      </md-table-row>
+
+
+
+    </md-table>
+  </div>
 </template>
 
 
@@ -9,9 +30,13 @@
 
   export default {
     name: 'Transactions',
-
+    data() {
+      return {
+        transactions: []
+      }
+    },
     methods: {
-      async method() {
+      async getTransactions() {
         let blockNum = await getCurrentBlockNumber();
         let transactions = []
         let blocks = 0
@@ -20,12 +45,16 @@
           transactions = transactions.concat(await getTransactions('0x' + (blockNum - count + 1).toString(16), '0x' + (blockNum).toString(16)));
           blockNum -= count;
         }
-
-        console.log(transactions);
-
+        return transactions; //truncate to 50
       }
+    },
+    created() {
+      this.getTransactions().then(transactions => {
+        this.transactions = transactions;
+      });
     }
   }
+
 
 
 

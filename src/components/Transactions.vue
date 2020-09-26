882ac49009027d84f54ca11d9626ed411c62e167
Brandon Li <brandon.li820@icloud.com>
2020-09-26 13:30:53 -0600

    use web3's getPastLogs

--- src/components/Transactions.vue
+++ src/components/Transactions.vue
@@ -25,8 +25,10 @@
 
 
 <script>
-  import { getCurrentBlockNumber, getTransactions } from '../api/infura.js';
+  import Web3 from 'Web3';
+  const web3 = new Web3(Web3.givenProvider);
 
+  const toHex = num => '0x' + (num).toString(16);
 
   export default {
     name: 'Transactions',
@@ -37,14 +39,22 @@
     },
     methods: {
       async getTransactions() {
-        let blockNum = await getCurrentBlockNumber();
+        const latest = await web3.eth.getBlockNumber();
+
         let transactions = []
         let blocks = 0
         const count = 1000;
         while (transactions.length <= 50) { //*page
-          transactions = await getTransactions('0x' + (blockNum - count*blocks).toString(16), '0x' + (blockNum).toString(16));
+
+          transactions = await web3.eth.getPastLogs({
+            fromBlock: toHex(latest - count * blocks),
+            toBlock: toHex(latest),
+            address: '0x07865c6E87B9F70255377e024ace6630C1Eaa37F'
+          } );
+
           blocks += 1;
         }
+
         transactions = transactions.slice(0, 50);
         transactions.forEach((transaction, index) => {
           transaction.id = index;
@@ -58,9 +68,5 @@
       });
     }
   }
-
-
-
-
 
 </script>

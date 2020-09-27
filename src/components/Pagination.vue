4f877d48db693eaff872e36c84e4e6ddfffd2335
Terrance Li <tyli@berkeley.edu>
2020-09-27 14:51:12 -0700

    Use md-icon for pagination

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,37 +1,45 @@
 <template>
-    <div class="page">
-        <md-button @click="changePage(0)">First</md-button>
-        <md-button @click="changePage(page - 1)">&lt;</md-button>
-        <p> Page {{ page }} of {{ totalPages }} </p>
-        <md-button @click="changePage(page + 1)">&gt;</md-button>
-        <md-button @click="changePage(totalPages)">Last</md-button>
-    </div>
+  <div class="page">
+    <md-button @click="changePage(0)">First</md-button>
+    <md-icon class="clickable" @click="changePage(page - 1)"
+      >navigate_before</md-icon
+    >
+    <p>Page {{ page }} of {{ totalPages }}</p>
+    <md-icon class="clickable" @click="changePage(page + 1)"
+      >navigate_next</md-icon
+    >
+    <md-button @click="changePage(totalPages)">Last</md-button>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'pagination',
-    props: {
-        totalPages: Number,
+  name: "pagination",
+  props: {
+    totalPages: Number,
+  },
+  data() {
+    return {
+      page: 0,
+    };
+  },
+  methods: {
+    changePage(page) {
+      page = Math.min(this.totalPages, Math.max(page, 0));
+      this.page = page;
+      this.$emit("page:change", page);
     },
-    data() {
-        return {
-            page: 0
-        }
-    },
-    methods: {
-        changePage(page) {
-            page = Math.min(this.totalPages, Math.max(page, 0));
-            this.page = page;
-            this.$emit('page:change', page);
-        }
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
+.clickable {
+  cursor: pointer;
+}
+
 .page {
-    display: flex;
-    flex-direction: row;
+  display: flex;
+  flex-direction: row;
 }
 </style>

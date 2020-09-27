0927e0cc8977f0313daab775c26f878ec96f3acd
unknown <tetsuroescudero@berkeley.edu>
2020-09-27 13:15:09 -0700

    Merging new table with transactions

--- src/pages/address/_address.vue
+++ src/pages/address/_address.vue
@@ -6,31 +6,14 @@
         <md-table-head>Sender Address</md-table-head>
         <md-table-head>Receiver Address</md-table-head>
         <md-table-head>How Long Ago</md-table-head>
-        <md-table-head>Transaction Amount</md-table-head>
-      </md-table-row>
-
-      <md-table-row>
-        <md-table-cell md-numeric>1</md-table-cell>
-        <md-table-cell>Shawna Dubbin</md-table-cell>
-        <md-table-cell>sdubbin0@geocities.com</md-table-cell>
-        <md-table-cell>Male</md-table-cell>
-        <md-table-cell>Assistant Media Planner</md-table-cell>
-      </md-table-row>
-
-      <md-table-row>
-        <md-table-cell md-numeric>2</md-table-cell>
-        <md-table-cell>Odette Demageard</md-table-cell>
-        <md-table-cell>odemageard1@spotify.com</md-table-cell>
-        <md-table-cell>Female</md-table-cell>
-        <md-table-cell>Account Coordinator</md-table-cell>
-      </md-table-row>
-
-      <md-table-row>
-        <md-table-cell md-numeric>3</md-table-cell>
-        <md-table-cell>Vera Taleworth</md-table-cell>
-        <md-table-cell>vtaleworth2@google.ca</md-table-cell>
-        <md-table-cell>Male</md-table-cell>
-        <md-table-cell>Community Outreach Specialist</md-table-cell>
+        <md-table-head>Transaction Amount</md-table-head> </md-table-row
+      >]
+      <md-table-row v-for="i in transactions" :key="i.transactioHash">
+        <md-table-cell></md-table-cell>
+        <md-table-cell></md-table-cell>
+        <md-table-cell></md-table-cell>
+        <md-table-cell></md-table-cell>
+        <md-table-cell></md-table-cell>
       </md-table-row>
     </md-table>
   </div>

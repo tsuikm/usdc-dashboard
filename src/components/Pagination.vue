e0498bad98abbe06d1bf079a76e8d8710af212d8
kelvinjue <35481358+kelvinjue@users.noreply.github.com>
2020-09-29 15:21:48 -0500

    Address transactions (#18)
    * Can now route to /address, plus console logs for tests
    
    * Formatting
    
    * Added example table
    
    * Added block traversal for transaction fetching by address
    
    * Fetch transaction data from Infura API
    
    * Use web3 for transaction lookup
    
    * Limit return to pagelength
    
    * Added binary search for start block and loading flag
    
    * Merging new table with transactions
    
    * Table now shows correct information
    
    * Fixed table showing multiple of same transaction
    
    * CHERRY-PICK: redo pagination
    
    * Use md-icon for pagination
    
    * Adding pagination, merging
    
    * Adding pagination, next and previous buttons do not work yet
    
    * Fix bug in Pagination, make it seem 1-indexed
    
    * Fix concatenation breaking chronological order
    
    * Pagination moved to top
    
    * Fix next and prev buttons in pagination
    
    * Add testing file with sanity check
    
    * Added testing
    
    * Fix code styling issues
    
    * Left align Transaction Hash header
    
    * Move totalPages into a computed property
    
    * Remove leading 0s on sender and receiver address
    
    * fixed lint error
    
    Co-authored-by: unknown <tetsuroescudero@berkeley.edu>
    Co-authored-by: Terrance Li <tyli@berkeley.edu>
    Co-authored-by: Constance Shi <constance.shi@berkeley.edu>

---
+++ src/components/Pagination.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="page">
+    <md-button @click="changePage(0)">First</md-button>
+    <md-button @click="changePage(page - 1)">
+      <md-icon>navigate_before</md-icon>
+    </md-button>
+    <p>Page {{ page + 1 }} of {{ totalPages }}</p>
+    <md-button @click="changePage(page + 1)">
+      <md-icon>navigate_next</md-icon>
+    </md-button>
+    <md-button @click="changePage(totalPages - 1)">Last</md-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "pagination",
+  props: {
+    totalPages: Number,
+  },
+  data() {
+    return {
+      page: 0,
+    };
+  },
+  methods: {
+    changePage(page) {
+      page = Math.min(this.totalPages - 1, Math.max(page, 0));
+      this.page = page;
+      this.$emit("page:change", page);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.clickable {
+  cursor: pointer;
+}
+
+.page {
+  display: flex;
+  flex-direction: row;
+}
+</style>

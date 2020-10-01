9886563d6f69899e02f5e9eb69fc46609ddab8af
Stephanie Xiao <60587269+stephaniexiao@users.noreply.github.com>
2020-10-01 14:54:18 -0700

    Fixed BalanceCard testing file to pass lint tests (#23)
    * balancecard testing passes lint tests
    
    * fixed balancecard test formatting
    
    Co-authored-by: Stephanie Xiao <stephaniexiao@Stephanies-MacBook-Pro.local>

--- tests/unit/BalanceCard.spec.js
+++ tests/unit/BalanceCard.spec.js
@@ -7,23 +7,21 @@ import VueMaterial from "vue-material";
 Vue.use(VueMaterial);
 
 describe("BalanceCard", () => {
-  it("ValueDisplay renders onto BalanceCard", async (done) => {
+  it("ValueDisplay renders onto BalanceCard", () => {
     const wrapper = mount(BalanceCard);
     const valueDisplay = wrapper.findComponent({ name: "ValueDisplay" });
     expect(valueDisplay.exists()).toBeTruthy();
     expect(valueDisplay.text()).toContain("BALANCE (USDC)");
     expect(valueDisplay.text()).toContain("BALANCE (USD $)");
-    done();
   });
 
-  it("Images display on BalanceCard", async (done) => {
+  it("Images display on BalanceCard", () => {
     const wrapper = mount(BalanceCard);
     const images = wrapper.find("img");
     expect(images.exists()).toBeTruthy();
-    done();
   });
 
-  it("ConversionDisplay renders onto BalanceCard", async (done) => {
+  it("ConversionDisplay renders onto BalanceCard", () => {
     const wrapper = mount(BalanceCard);
     const conversionDisplay = wrapper.findComponent({
       name: "ConversionDisplay",
@@ -32,21 +30,20 @@ describe("BalanceCard", () => {
     expect(conversionDisplay.text()).toContain("CONVERSION RATE:");
     expect(conversionDisplay.text()).toContain("1 USD//Coin to");
     expect(conversionDisplay.text()).toContain("US Dollar");
-    done();
   });
 });
 
 describe("Dashboard", () => {
-  it("BalanceCard displays on Dashboard input's submit", async (done) => {
+  it("BalanceCard displays on Dashboard input's submit", async () => {
     const wrapper = mount(Dashboard);
     const addr = wrapper.find("input");
     const form = wrapper.find("form");
+    
     await addr.setValue("0xc0539c310393165705265dc9865a0E495202771B");
     await form.trigger("submit.prevent");
     await wrapper.vm.$nextTick();
 
     const balancecard = wrapper.findComponent({ name: "BalanceCard" });
     expect(balancecard.exists()).toBeTruthy();
-    done();
   });
 });

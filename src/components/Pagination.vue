a10e096adb1542e3507c278a565ae2d715abaa1d
Terrance Li <tyli@berkeley.edu>
2020-09-30 18:20:45 -0700

    Quick fix for pagination going 1 over max

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -14,28 +14,28 @@
 
 <script>
 export default {
-  name: 'Pagination',
+  name: "Pagination",
   props: {
     totalPages: Number,
   },
   data() {
     return {
-      page: 0
-    }
+      page: 0,
+    };
   },
   methods: {
     changePage(page) {
-      page = Math.min(this.totalPages, Math.max(page, 0));
+      page = Math.min(this.totalPages - 1, Math.max(page, 0));
       this.page = page;
-      this.$emit('page:change', page);
-    }
-  }
-}
+      this.$emit("page:change", page);
+    },
+  },
+};
 </script>
 
 <style scoped>
 .page {
-    display: flex;
-    flex-direction: row;
+  display: flex;
+  flex-direction: row;
 }
 </style>

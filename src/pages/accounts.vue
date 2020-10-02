cb9437e2386e9a155dd454bd729faa2fe30b3550
Brandon Li <brandon.li820@icloud.com>
2020-10-02 12:28:59 -0600

    spacing

--- src/pages/accounts.vue
+++ src/pages/accounts.vue
@@ -17,59 +17,45 @@
 import Table from "@/components/Table";
 export default {
   components: {
-    Table,
+    Table
   },
   computed: {
     totalItems() {
-        // TODO: this number is hard-coded. We need to calculate the total number of transactions
-        if (!this.address) return 5500;
-
-        return this.transactions.length;
-        },
-        tableSchema() {
-        // const transactionSchema = {
-        //   "Transaction Hash": (t) => t.transactionHash,
-        //   Quantity: (t) => t.data,
-        //   Sender: (t) => ({
-        //     value: t.from,
-        //     link: `/address/${t.from}`,
-        //   }),
-        //   Receiver: (t) => ({
-        //     value: t.to,
-        //     link: `/address/${t.to}`,
-        //   }),
-        // };
-        return [
-                {
-                name: "Address",
-                getter(account) {
-                    return account.address;
-                },
-                link(account) {
-                    return `/address/${account.address}`;
-                },
-                },
-                {
-                name: "Balance",
-                getter(account) {
-                    return account.balance;
-                },
-                },
-                {
-                name: "Percentage",
-                getter(account) {
-                    return account.percentage;
-                },
-                },
-            ];
-            },
-        pageLength() {
-            return this.$refs.table.pageLength;
-        },
+      // TODO: this number is hard-coded. We need to calculate the total number of transactions
+      return 5500;
     },
+    tableSchema() {
+      return [
+        {
+          name: 'Address',
+          getter(account) {
+              return account.address;
+          },
+          link(account) {
+              return `/address/${account.address}`;
+          },
+        },
+        {
+          name: 'Balance',
+          getter(account) {
+              return account.balance;
+          },
+        },
+        {
+          name: 'Percentage',
+          getter(account) {
+              return account.percentage;
+          }
+        }
+      ];
+    },
+    pageLength() {
+        return this.$refs.table.pageLength;
+    }
+  },
   data() {
     return {
-      page: 0,
+      page: 0
     };
   }
 };

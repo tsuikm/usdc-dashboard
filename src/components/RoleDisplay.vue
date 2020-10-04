4f53f39933bc54614a64e2c4523094db43581403
Stephanie Xiao <stephaniexiao@Stephanies-MacBook-Pro.local>
2020-10-04 14:48:29 -0700

    replaced chip component

--- src/components/RoleDisplay.vue
+++ src/components/RoleDisplay.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="role-container">
       <div v-if="this.minter" >
-          <v-chip class="chip">
+          <md-chip class="chip">
             MINTER
-          </v-chip>
+          </md-chip>
       </div>
       <div v-if="this.pauser">
-          <v-chip class="chip">
+          <md-chip class="chip">
             PAUSER
-          </v-chip>
+          </md-chip>
       </div>
       <div v-if="this.owner">
-          <v-chip class="chip">
+          <md-chip class="chip">
             OWNER
-          </v-chip>
+          </md-chip>
       </div>
   </div>
 </template>
@@ -32,8 +32,6 @@ export default {
 <style scoped>
 .chip {
     background-color: #68d7f3;
-    padding: 8px;
-    border-radius: 10px;
     margin-left: 20px;
     color: white;
     font-weight: 700;

2695b43d5f73504d375591ab0d6ddacb5751ae46
Constance Shi <constance.shi@berkeley.edu>
2020-10-06 22:38:05 -0700

    remove duplicate navbar

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,21 +1,5 @@
 <template>
   <div>
-    <md-toolbar>
-      <div class="md-toolbar-row">
-        <div class="md-toolbar-section-start">
-          <img id="logo" src="@/assets/usdc-logo.png" />
-        </div>
-        <div class="md-toolbar-section-end">
-          <form @submit.prevent="submitAddress">
-            <md-field>
-              <label>Wallet Address</label>
-              <md-input type="text" v-model="walletAddress"></md-input>
-              <md-icon>search</md-icon>
-            </md-field>
-          </form>
-        </div>
-      </div>
-    </md-toolbar>
     <NavBar />
     <Transactions />
   </div>

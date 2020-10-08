4ed7fc49e9d046499ddc50e1693659aa13a9a7f6
Stephanie Xiao <stephaniexiao@Stephanies-MacBook-Pro.local>
2020-10-08 04:13:26 -0700

    fixed usdc balance header

--- src/components/ValueDisplay.vue
+++ src/components/ValueDisplay.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="md-layout">
     <div class="md-layout-items left">
-        <div class="subheading">BALANCE (USDC)</div>
+        <div class="subheading">USDC BALANCE ($)</div>
         <div class="value">{{ usdcBalance }}</div>
     </div>
   </div>

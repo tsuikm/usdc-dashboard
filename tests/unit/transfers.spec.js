900d9b2b66695e364fef73b12c717838e4b7bc77
unknown <tetsuroescudero@berkeley.edu>
2020-10-17 20:52:24 -0700

    Added page for testing transfers

---
+++ tests/unit/transfers.spec.js
@@ -0,0 +1,12 @@
+import transfers from '@/pages/transfers/index';
+import Vue from "vue";
+import VueMaterial from 'vue-material';
+
+Vue.use(VueMaterial);
+
+describe('Transfers page', () => {
+test('Transfers card renders', async () => {
+    
+})    
+
+})

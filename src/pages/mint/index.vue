98e33315b1fc0ad7c2665c97982bd5548315b0df
Brandon Li <brandon.li820@icloud.com>
2020-10-21 23:48:32 -0600

    front end

--- src/pages/mint/index.vue
+++ src/pages/mint/index.vue
@@ -1,12 +1,29 @@
 <template>
   <div>
-    <NavBar />
-    <form @submit.prevent="mint">
-        <input placeholder='Wallet address:' v-model="walletAddress" />
-        <input placeholder='To address:' v-model="toAddress" />
-        <input placeholder='amount:' v-model="amount" />
-        <button>Submit mint event</button>
-    </form>
+    <md-card class="card">
+      <md-card-header class="header">
+        <div class="md-title">Mint USDC</div>
+      </md-card-header>
+
+      <label>Wallet address:</label>
+      <md-field class="field">
+        <md-input class="input" v-model="walletAddress"></md-input>
+      </md-field>
+
+      <label>To address:</label>
+      <md-field class="field">
+        <md-input class="input" v-model="toAddress"></md-input>
+      </md-field>
+
+
+      <label>Amount:</label>
+      <md-field class="field">
+        <md-input class="input" v-model="amount"></md-input>
+      </md-field>
+      <md-card-actions>
+        <md-button>Send</md-button>
+      </md-card-actions>
+    </md-card>
   </div>
 </template>
 
@@ -48,18 +65,84 @@ export default {
         amount: 0
     };
   },
+
+  head() {
+    return {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css?family=Proxima+Nova&display=swap'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap'
+        }
+      ]
+    }
+  },
+
   async created() {
   },
   methods: {
-      async mint() {
-        if (! await contract.methods.isMinter(padHex(this.walletAddress, WEB3_BALANCEOF_ADDRESS_LENGTH)).call()) {
-          //not allowed to mint
-          console.log("not allowed");
-          return;
-        } else {
-          console.log(await contract.methods.mint(this.toAddress, this.amount).call());
-        }
+    async mint() {
+      if (! await contract.methods.isMinter(padHex(this.walletAddress, WEB3_BALANCEOF_ADDRESS_LENGTH)).call()) {
+        //not allowed to mint
+        console.log("not allowed");
+        return;
+      } else {
+        console.log(await contract.methods.mint(this.toAddress, this.amount).call());
       }
     }
-  };
+  }
+};
 </script>
+
+<style scoped>
+.input {
+  width: 25%;
+  left: 37.5%;
+  box-sizing: border-box;
+  border-radius: 5px;
+  display: block;
+  text-align: left;
+  margin-bottom: 14px;
+  font-family: Roboto Mono;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 18px;
+  letter-spacing: 0.03em;
+  color: #717171;
+}
+
+.card {
+  width: 50%;
+  left: 25%;
+  padding: 5%;
+  background-color: #FFF;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.15);
+  border-radius: 16px;
+}
+
+label {
+  font-family: Proxima Nova;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 11px;
+  line-height: 13px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  color: #3D3652;
+  display: block;
+}
+
+.header {
+  color: white;
+}
+
+.field {
+  border: 1px solid #DBDCDC;
+  box-sizing: border-box;
+  border-radius: 5px;
+}
+</style>

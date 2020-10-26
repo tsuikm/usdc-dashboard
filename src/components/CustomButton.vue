dbe1e628b5e4d25e200c77a82dc768198533066e
Kelly Tsai <kellytsai@berkeley.edu>
2020-10-27 07:16:41 +0800

    created custom button

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="button">
+    <button
+      :class="{'button-colored': minter, 'button-gray': !minter}"
+      data-testid="minter"
+      @click="clickMinter"
+    >
+      MINTER
+    </button>
+    <button
+      :class="{'button-colored': pauser, 'button-gray': !pauser}"
+      data-testid="pauser"
+      @click="clickPauser"
+    >
+      PAUSER
+    </button>
+    <button
+      :class="{'button-colored': owner, 'button-gray': !owner}"
+      data-testid="owner"
+      @click="clickOwner"
+    >
+      OWNER
+    </button>
+    <button
+      :class="{'button-colored': blacklister, 'button-gray': !blacklister}"
+      data-testid="blacklister"
+      @click="clickBlacklister"
+    >
+      BLACKLISTER
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CustomButton',
+  props:{
+    minter: Boolean,
+    pauser: Boolean,
+    owner: Boolean,
+    blacklister: Boolean,
+  },
+  methods: {
+    clickMinter() {
+      let updatedMinter = !this.minter;
+      this.$emit("update-role", updatedMinter, this.pauser, this.owner, this.blacklister);
+    },
+    clickPauser() {
+      let updatedPauser = !this.pauser;
+      this.$emit("update-role", this.minter, updatedPauser, this.owner, this.blacklister);
+    },
+    clickOwner() {
+      let updatedOwner = !this.owner;
+      this.$emit("update-role", this.minter, this.pauser, updatedOwner, this.blacklister);
+    },
+    clickBlacklister() {
+      let updatedBlacklister= !this.blacklister;
+      this.$emit("update-role", this.minter, this.pauser, this.owner, updatedBlacklister);
+    },
+  }
+}
+</script>
+
+<style scoped>
+.button {
+  margin-bottom: 5px;
+}
+
+.button-colored {
+  background-color: #68d7f3;
+  margin: 10px;
+  padding: 15px;
+  color: white;
+  font-weight: 700;
+  float: left;
+  border-radius: 25px;
+  border: none;
+}
+
+.button-gray {
+  background-color: #d1d1d1;
+  margin: 10px;
+  padding: 15px;
+  color: white;
+  font-weight: 700;
+  float: left;
+  border-radius: 25px;
+  border: none;
+}
+</style>
--- src/components/OwnerControl.vue
+++ src/components/OwnerControl.vue
@@ -15,76 +15,13 @@
       </md-field>
     </form>
     <div class="role-control">
-      <div class="role-container">
-        <div v-if="this.minter">
-          <button
-            class="button-colored"
-            data-testid="minter"
-            @click="clickMinter"
-          >
-            MINTER
-          </button>
-        </div>
-        <div v-else>
-          <button
-            class="button-gray"
-            @click="clickMinter"
-          >
-            MINTER
-          </button>
-        </div>
-        <div v-if="this.pauser">
-          <button
-            class="button-colored"
-            data-testid="pauser"
-            @click="clickPauser"
-          >
-            PAUSER
-          </button>
-        </div>
-        <div v-else>
-          <button
-            class="button-gray"
-            @click="clickPauser"
-          >
-            PAUSER
-          </button>
-        </div>
-        <div v-if="this.owner">
-          <button
-            class="button-colored"
-            data-testid="owner"
-            @click="clickOwner"
-          >
-            OWNER
-          </button>
-        </div>
-        <div v-else>
-          <button
-            class="button-gray"
-            @click="clickOwner"
-          >
-            OWNER
-          </button>
-        </div>
-        <div v-if="this.blacklister">
-          <button
-            class="button-colored"
-            data-testid="blacklister"
-            @click="clickBlacklister"
-          >
-            BLACKLISTER
-          </button>
-        </div>
-        <div v-else>
-          <button
-            class="button-gray"
-            @click="clickBlacklister"
-          >
-            BLACKLISTER
-          </button>
-        </div>
-      </div>
+      <CustomButton 
+        :minter="this.minter"
+        :pauser="this.pauser"
+        :owner="this.owner"
+        :blacklister="this.blacklister"
+        @update-role="update"
+      />
       <div class="update-button">
         <md-button @click="updateRoles">
           SAVE
@@ -99,12 +36,16 @@ import { USDC_CONTRACT_ADDRESS, WEB3_BALANCEOF_ADDRESS_LENGTH, BLACKLISTER_ADDRE
 import Web3 from 'web3';
 import { padHex } from '@/utils/utils';
 import { abi } from '@/utils/web3abi';
+import CustomButton from '@/components/CustomButton';
 
 const web3 = new Web3(Web3.givenProvider);
 const contract = new web3.eth.Contract(abi, USDC_CONTRACT_ADDRESS);
 
 export default {
   name: 'Owner',
+  components: {
+    CustomButton,
+  },
   data() {
     return {
       address: '',
@@ -112,10 +53,6 @@ export default {
       pauser: null,
       owner: null,
       blacklister: null,
-      newMinter: null,
-      newPauser: null,
-      newOwner: null,
-      newBlacklister: null,
     };
   },
   methods: {
@@ -147,45 +84,13 @@ export default {
       this.checkIsPauser();
       this.checkIsOwner();
     },
-    clickMinter() {
-      this.newMinter = !this.newMinter;
-    },
-    clickPauser() {
-      this.newPauser = !this.newPauser;
-    },
-    clickOwner() {
-      this.newOwner = !this.newOwner;
-    },
-    clickBlacklister() {
-      this.newBlacklister = !this.blacklister;
+    update(minter, pauser, owner, blacklister) {
+      this.minter = minter;
+      this.puaser = pauser;
+      this.owner = owner;
+      this.blacklister = blacklister;
     },
     updateRoles() {
-      if (this.minter === true && this.newMinter === false) {
-        //unminter the address
-      }
-      if (this.minter === false && this.newMinter === true) {
-        //create minter
-      }
-      if (this.pauser === true && this.newPauser=== false) {
-        //unpauser the address
-      }
-      if (this.pauser === false && this.newPauser === true) {
-        //create pauser 
-      }
-      if (this.owner === true && this.newOwner=== false) {
-        //unowner the address
-      }
-      if (this.owner === false && this.newOwner === true) {
-        //create owner
-      }
-      if (this.blacklister === true && this.newBlacklister=== false) {
-        //unblacklister the address
-        this.blacklister = false;
-      }
-      if (this.blacklister === false && this.newBlacklister === true) {
-        //create blacklister
-        this.blacklister = true;
-      }
     },
   },
 };
@@ -197,17 +102,13 @@ export default {
   margin: 40px;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.15);
   border-radius: 10px;
-  width: 50%;
+  width: 60%;
 }
 
 .header {
   font-size: 20px;
   font-weight: 900;
   padding-bottom: 3%;
-}
-
-.button {
-  margin-bottom: 5px;
 }
 
 .input-form {
@@ -222,27 +123,5 @@ export default {
 
 .role-container {
   display: flex;
-}
-
-.button-colored {
-  background-color: #68d7f3;
-  margin: 10px;
-  padding: 15px;
-  color: white;
-  font-weight: 700;
-  float: left;
-  border-radius: 25px;
-  border: none;
-}
-
-.button-gray {
-  background-color: #d1d1d1;
-  margin: 10px;
-  padding: 15px;
-  color: white;
-  font-weight: 700;
-  float: left;
-  border-radius: 25px;
-  border: none;
 }
 </style>

9851ccb54e964adacc5b1cfa4a0df468bc4834d4
Stephanie Xiao <stephaniexiao@Stephanies-MacBook-Pro.local>
2020-10-26 04:13:48 -0700

    creating custom button component, in progress

--- src/components/RoleButton.vue
+++ src/components/RoleButton.vue
@@ -1,9 +1,22 @@
 <template>
   <div>
+    <button
+      class="button-colored"
+      data-testid="minter"
+      @click="clickMinter">
+        {{ title }}
+    </button>
   </div>
 </template>
 
 <script>
+export default {
+  name: 'RoleButton',
+  props: {
+    testId: String,
+    title: String,
+  }
+}
 </script>
 
 <style scoped>

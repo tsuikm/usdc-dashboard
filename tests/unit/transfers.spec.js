1372a555a8312060d4149e8c41629d4b07214876
Terrance Li <terranceli.terbear@gmail.com>
2020-10-26 23:11:33 -0700

    Working on mock for metamask

--- tests/unit/transfers.spec.js
+++ tests/unit/transfers.spec.js
@@ -1,9 +1,13 @@
-import { render } from '@testing-library/vue';
+import { render, fireEvent } from '@testing-library/vue';
 import transfers from '@/pages/transfers/index';
 import Vue from 'vue';
 import VueMaterial from 'vue-material';
 
 Vue.use(VueMaterial);
+
+global.ethereum = {
+  request: jest.fn(),
+};
 
 describe('Transfers page', () => {
   test('Transfers card renders', async () => {
@@ -12,7 +16,18 @@ describe('Transfers page', () => {
     expect(queryByTestId('transfers-card-to-input')).not.toBeNull();
     expect(queryByTestId('transfers-card-from-input')).not.toBeNull();
     expect(queryByTestId('transfers-card-amount-input')).not.toBeNull();
+    expect(queryByTestId('transfers-card-connect-button')).not.toBeNull();
     expect(queryByTestId('transfers-card-send-button')).not.toBeNull();
-  });    
+  });
+  
+  test('Connect Metamask button works', async () => {
+    const { queryByTestId } = render(transfers);
+    const button = queryByTestId('transfers-card-connect-button');
+    await fireEvent.click(button);
 
+    // eslint-disable-next-line
+    expect(ethereum.request.mock.calls[0]).toEqual([{ method: 'eth_requestAccounts' }]);
+    // eslint-disable-next-line
+    expect(ethereum.request.mock.calls).toHaveLength(1);
+  });
 });

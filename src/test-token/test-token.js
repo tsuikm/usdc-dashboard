de846c70b3f1cebbc37f37e50cdd8020f5f643a0
Constance Shi <constance.shi@berkeley.edu>
2020-10-31 20:36:34 -0700

    try to run js files and debug test token

--- src/test-token/test-token.js
+++ src/test-token/test-token.js
@@ -1,24 +1,29 @@
-import { abi } from '@/constants/testTokenABI';
-import Web3 from web3;
-import { TEST_TOKEN_CONTRACT_ADDRESS, TEST_TOKEN_OWNER_ADDRESS } from '../utils/constants';
+import { abi } from '../utils/testTokenABI.js';
+import Web3 from 'web3';
+import { TEST_TOKEN_CONTRACT_ADDRESS, TEST_TOKEN_OWNER_ADDRESS } from '../utils/constants.js';
 
+console.log("line 5");
 const web3 = new Web3(Web3.givenProvider);
+console.log("line 7");
 const contract = new web3.eth.Contract(abi, TEST_TOKEN_CONTRACT_ADDRESS);
+console.log("line 9");
 
 const addMinter = async address => {
+    console.log("line 12");
     await contract.methods.configureMinter(address, 5000).call();
-    return contract.methods.isMinter(address);
+    console.log("line 14");
+    console.log(contract.methods.isMinter(address));
 }
 
-const pause = async => {
+const pause = async () => {
     await contract.methods.pause().call();
 }
 
-const unpause = async => {
+const unpause = async () => {
     await contract.methods.unpause().call();
 }
 
-return addMinter(TEST_TOKEN_OWNER_ADDRESS);
+addMinter(TEST_TOKEN_OWNER_ADDRESS);
 //addMinter();
 
 

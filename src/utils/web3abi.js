e079000e30e357f85576435830251dc7ff1b436f
Brandon Li <brandon.li820@icloud.com>
2020-11-04 23:31:50 -0700

    add pause/unpause abi functions

--- src/utils/web3abi.js
+++ src/utils/web3abi.js
@@ -60,4 +60,18 @@ export const abi = [
     outputs: [{ name: '', type: 'bool'}],
     type: 'function',
   },
+  {
+    inputs: [],
+    name: 'pause',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    inputs: [],
+    name: 'unpause',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
 ];

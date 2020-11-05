5c1dcf7ee34fcc175fe7b65692fab8aeb42a8d5a
Brandon Li <brandon.li820@icloud.com>
2020-11-05 00:01:46 -0700

    add pausable func to LocalERC20

--- contracts/LocalERC20.sol
+++ contracts/LocalERC20.sol
@@ -1,8 +1,9 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol';
 
-contract LocalERC20 is ERC20 {
+contract LocalERC20 is ERC20, ERC20Pausable {
 
   /**
    * Implementation of the {IERC20} interface for local development, using
@@ -17,5 +18,9 @@ contract LocalERC20 is ERC20 {
 
     // Use 6 as the decimals.
     _setupDecimals(6);
+  }
+
+  function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override(ERC20, ERC20Pausable) {
+    super._beforeTokenTransfer(from, to, amount);
   }
 }

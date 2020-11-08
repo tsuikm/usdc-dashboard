a9bdc079c0672277ded36d59e1a407d273bcceca
Brandon Li <brandon.li820@icloud.com>
2020-11-07 21:33:13 -0700

    Revert "add abi methods to mock"
    This reverts commit d0f84e49d8065a97e9bf52523cf95c55c264510e.

--- src/components/OwnerControl.vue
+++ src/components/OwnerControl.vue
@@ -15,6 +15,9 @@
       </md-field>
     </form>
     <div class="role-control">
+      <div v-if="!hasRoles && address !== ''">
+        Address has no roles.
+      </div>
       <div class="role-button-row">
         <RoleButton
           :title="this.minterTitle"
@@ -37,8 +40,11 @@
           :on-click="clickBlacklister"
         />
       </div>
+      <div v-if="hasRenouncedRoles">
+        ⚠️ Warning: Cannot renounce roles. Please assign role to another address.
+      </div>
       <div class="update-button">
-        <md-button>
+        <md-button @click="this.save">
           SAVE
         </md-button>
       </div>
@@ -47,13 +53,52 @@
 </template>
 
 <script>
-import { USDC_CONTRACT_ADDRESS, WEB3_BALANCEOF_ADDRESS_LENGTH, BLACKLISTER_ADDRESS } from '@/utils/constants';
+import { USDC_CONTRACT_ADDRESS, WEB3_BALANCEOF_ADDRESS_LENGTH, DEFAULT_GAS_PRICE } from '@/utils/constants';
 import Web3 from 'web3';
 import { padHex } from '@/utils/utils';
 import { abi } from '@/utils/web3abi';
 import RoleButton from '@/components/RoleButton';
+
 const web3 = new Web3(Web3.givenProvider);
 const contract = new web3.eth.Contract(abi, USDC_CONTRACT_ADDRESS);
+
+/*----------------------------------------------------------------------------*
+ * Helpers
+ *----------------------------------------------------------------------------*/
+
+/**
+ * Getters for the addresses of each role (ie. master minter, pauser, owner, blacklister).
+ *
+ * @returns {String} - as a lowercase hex string.
+ */
+async function getOwner() { return contract.methods.owner().call(); }
+async function getPauser() { return contract.methods.pauser().call(); }
+async function getBlacklister() { return contract.methods.blacklister().call(); }
+async function getMasterMinter() { return contract.methods.masterMinter().call(); }
+
+/**
+ * [changeRole description]
+ * @param {[type]} ownerAccount [description]
+ * @param {[type]} contractMethod [description]
+ * @param {[type]} address [description]
+ * @returns {[type]} [description]
+ */
+async function changeRole(ownerAccount, contractMethod, address) {
+  await ethereum.request({
+    method: 'eth_sendTransaction',
+    params: [
+      {
+        from: ownerAccount,
+        to: USDC_CONTRACT_ADDRESS,
+        data: contractMethod(address).encodeABI(),
+        gasPrice: DEFAULT_GAS_PRICE
+      },
+    ],
+  });
+}
+
+//----------------------------------------------------------------------------------------
+
 export default {
   name: 'Owner',
   components: {
@@ -66,30 +111,51 @@ export default {
       pauser: null,
       owner: null,
       blacklister: null,
-      minterTitle: 'MINTER',
+      minterTitle: 'MASTER MINTER',
       pauserTitle: 'PAUSER',
       ownerTitle: 'OWNER',
       blacklisterTitle: 'BLACKLISTER',
+      hasRenouncedRoles: null,
+
+
+      blacklisterAddress: null,
+      ownerAddress: null,
+      masterMinterAddress: null,
+      pauserAddress: null
     };
+  },
+  computed: {
+    hasRoles() {
+      // const array = [
+      //   this.blacklisterAddress,
+      //   this.masterMinterAddress,
+      //   this.ownerAddress,
+      //   this.pauserAddress
+      // ].map(string => string.toLowerCase());
+      // console.log(array, this.address.toLowerCase())
+
+      // const hasRoles = array.includes(this.address.toLowerCase());
+      // console.log(hasRoles)
+      // return hasRoles;
+      return true;
+    }
   },
   methods: {
     async lookupBlacklisted() {
-      if (this.address === '') {
-        return;
-      }
-      this.blacklister = this.address === BLACKLISTER_ADDRESS;
+      this.blacklisterAddress = await contract.methods.blacklister().call();
+      this.blacklister = this.address === this.blacklisterAddress;
     },
     async checkIsMinter() {
-      this.minter = await contract.methods.isMinter(this.address).call();
+      this.minterAddress = await contract.methods.masterMinter().call()
+      this.minter = this.address === this.minterAddress;
     },
     async checkIsPauser() {
-      const pauserAddress = await contract.methods.pauser().call();
-      this.pauser = pauserAddress === this.address;
+      this.pauserAddress = await contract.methods.pauser().call();
+      this.pauser = this.pauserAddress === this.address;
     },
     async checkIsOwner() {
-      const owner = await contract.methods.owner().call();
-      const ownerAddress = padHex(owner, WEB3_BALANCEOF_ADDRESS_LENGTH);
-      this.owner = ownerAddress === this.address;
+      this.ownerAddress = await contract.methods.owner().call();
+      this.owner = this.ownerAddress === this.address;
     },
     checkRoles() {
       this.lookupBlacklisted();
@@ -109,6 +175,53 @@ export default {
     clickBlacklister() {
       this.blacklister= !this.blacklister;
     },
+    async changeRole(ownerAccount, method) {
+      await ethereum.request({
+        method: 'eth_sendTransaction',
+        params: [
+          {
+            from: ownerAccount,
+            to: USDC_CONTRACT_ADDRESS,
+            data: method(this.address).encodeABI(),
+            gasPrice: DEFAULT_GAS_PRICE
+          },
+        ],
+      });
+    },
+    async save() {
+      this.checkRoles();
+      const accounts = (await ethereum.request({ method: 'eth_requestAccounts' })).map(string => string.toLowerCase());
+      const ownerAccount = await contract.methods.owner().call();
+
+      if (!accounts.includes(ownerAccount.toLowerCase())) {
+        console.log('You are not the owner');
+        return;
+      }
+
+      this.hasRenouncedRoles = (!this.pauser && await contract.methods.pauser().call() === this.address) ||
+        (!this.minter && await contract.methods.masterMinter().call() === this.address) ||
+        (!this.blacklister && await contract.methods.blacklister().call() === this.address) ||
+        (!this.owner && await contract.methods.owner().call() === this.address);
+
+      if (this.hasRenouncedRoles) {
+        return;
+      }
+
+
+      if (this.pauser && await contract.methods.pauser().call() !== this.address) {
+        await this.changeRole(ownerAccount, contract.methods.updatePauser);
+      }
+      if (this.minter && await contract.methods.masterMinter().call() !== this.address) {
+        await this.changeRole(ownerAccount, contract.methods.updateMasterMinter);
+      }
+      if (this.blacklister && await contract.methods.blacklister().call() !== this.address) {
+        await this.changeRole(ownerAccount, contract.methods.updateBlacklister);
+      }
+      if (this.owner && await contract.methods.owner().call() !== this.address) {
+        await this.changeRole(ownerAccount, contract.methods.transferOwnership);
+      }
+
+    }
   },
 };
 </script>
@@ -121,22 +234,27 @@ export default {
   border-radius: 10px;
   width: 60%;
 }
+
 .header {
   font-size: 20px;
   font-weight: 900;
   padding-bottom: 3%;
 }
+
 .input-form {
   align-items: center;
 }
+
 .role-control {
   display: flex;
   flex-direction: column;
   align-items: center;
 }
+
 .role-button-row {
   display: flex;
 }
+
 .role-container {
   display: flex;
 }

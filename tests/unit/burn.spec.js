b16935dabce910b56ae1f49c240a7a137730ba5c
Kelly Tsai <kellytsai@berkeley.edu>
2020-11-10 14:51:48 +0800

    test unable to send mint/burn request

--- tests/unit/burn.spec.js
+++ tests/unit/burn.spec.js
@@ -1,9 +1,9 @@
 import { render, fireEvent } from '@testing-library/vue';
-import mint from '@/pages/mint/index';
+import burn from '@/pages/burn/index';
 import Vue from 'vue';
 import VueMaterial from 'vue-material';
-// import { USDC_CONTRACT_ADDRESS, DEFAULT_GAS_PRICE } from '@/utils/constants';
-// import { toHex } from '@/utils/utils';
+import { USDC_CONTRACT_ADDRESS, DEFAULT_GAS_PRICE } from '@/utils/constants';
+import { toHex } from '@/utils/utils';
 Vue.use(VueMaterial);
 
 const MOCK_WALLET_ADDRESS = '0xdC1e071D120FD40fB1173BCcc86c74F47645F4E0';
@@ -12,44 +12,39 @@ global.ethereum = {
   request: jest.fn(async () => [MOCK_WALLET_ADDRESS]),
 };
 
-describe('Mint page', () => {
-  test('Mint card renders', async () => {
-    const { queryByLabelText, queryByText } = render(mint);
-    expect(queryByText('Mint USDC')).not.toBeNull();
-    expect(queryByLabelText('To Address')).not.toBeNull();
-    expect(queryByLabelText('Amount')).not.toBeNull();
+describe('Burn page', () => {
+  test('Burn card renders', async () => {
+    const { queryByTestId, queryByText } = render(burn);
+    expect(queryByText('Burn USDC')).not.toBeNull();
+    expect(queryByTestId('Amount')).not.toBeNull();
     expect(queryByText('Send')).not.toBeNull();
   });
 
-  test('Mint button works', async () => {
-    const { queryByLabelText, queryByText } = render(mint);
+  test('Burn button works', async () => {
+    const { queryByTestId, queryByText } = render(burn);
 
     // eslint-disable-next-line
     expect(ethereum.request.mock.calls[0]).toEqual([{ method: 'eth_requestAccounts' }]);
     // eslint-disable-next-line
-    // expect(ethereum.request.mock.calls).toHaveLength(1);
-    const TO_WALLET_ADDRESS = '0x12345';
+    expect(ethereum.request.mock.calls).toHaveLength(2);
     const AMOUNT_TEXT = '100';
-
     const sendButton = queryByText('Send');
-    const amountInput = queryByLabelText('Amount');
-    const toInput = queryByLabelText('To Address');
+    const amountInput = queryByTestId('Amount');
 
-    await fireEvent.update(toInput, TO_WALLET_ADDRESS);
     await fireEvent.update(amountInput, AMOUNT_TEXT);
     await fireEvent.click(sendButton);
 
     // eslint-disable-next-line
-    // expect(ethereum.request.mock.calls[2]).toEqual([{
-    //   method: 'eth_sendTransaction',
-    //   params: [
-    //     {
-    //       from: MOCK_WALLET_ADDRESS,
-    //       to: USDC_CONTRACT_ADDRESS,
-    //       data: TO_WALLET_ADDRESS + ', ' + toHex(Number(AMOUNT_TEXT) * 1000000),
-    //       gasPrice: DEFAULT_GAS_PRICE,
-    //     },
-    //   ],
-    // }]);
+    expect(ethereum.request.mock.calls[2]).toEqual([{
+      method: 'eth_sendTransaction',
+      params: [
+        {
+          from: MOCK_WALLET_ADDRESS,
+          to: USDC_CONTRACT_ADDRESS,
+          data: toHex(Number(AMOUNT_TEXT) * 1000000),
+          gasPrice: DEFAULT_GAS_PRICE,
+        },
+      ],
+    }]);
   });
-});
+}); 
--- tests/unit/mint.spec.js
+++ tests/unit/mint.spec.js
@@ -1,51 +1,70 @@
 import { render, fireEvent } from '@testing-library/vue';
-import burn from '@/pages/burn/index';
+import mint from '@/pages/mint/index';
 import Vue from 'vue';
 import VueMaterial from 'vue-material';
-// import { USDC_CONTRACT_ADDRESS, DEFAULT_GAS_PRICE } from '@/utils/constants';
-// import { toHex } from '@/utils/utils';
+import { USDC_CONTRACT_ADDRESS, DEFAULT_GAS_PRICE } from '@/utils/constants';
+import { toHex } from '@/utils/utils';
+// import Web3 from 'web3';
 Vue.use(VueMaterial);
 
+// const MOCK_ACCOUNTS = {
+//   '0x12345': {
+//     balance: 10000,
+//     minter: true,
+//     pauser: false,
+//     owner: false,
+//     blacklisted: false,
+//   },
+// };
 const MOCK_WALLET_ADDRESS = '0xdC1e071D120FD40fB1173BCcc86c74F47645F4E0';
+// Web3.MOCK_ACCOUNTS = MOCK_ACCOUNTS;
 
 global.ethereum = {
   request: jest.fn(async () => [MOCK_WALLET_ADDRESS]),
 };
 
-describe('Burn page', () => {
-  test('Burn card renders', async () => {
-    const { queryByLabelText, queryByText } = render(burn);
-    expect(queryByText('Burn USDC')).not.toBeNull();
-    expect(queryByLabelText('Amount')).not.toBeNull();
+describe('Mint page', () => {
+  test('Mint card renders', async () => {
+    const { queryByTestId, queryByText } = render(mint);
+    expect(queryByText('Mint USDC')).not.toBeNull();
+    expect(queryByTestId('To Address')).not.toBeNull();
+    expect(queryByTestId('Amount')).not.toBeNull();
     expect(queryByText('Send')).not.toBeNull();
   });
 
-  test('Burn button works', async () => {
-    const { queryByLabelText, queryByText } = render(burn);
+  test('Mint button works', async () => {
+    const { queryByTestId, queryByText } = render(mint);
 
     // eslint-disable-next-line
     expect(ethereum.request.mock.calls[0]).toEqual([{ method: 'eth_requestAccounts' }]);
     // eslint-disable-next-line
-    // expect(ethereum.request.mock.calls).toHaveLength(1);
+    expect(ethereum.request.mock.calls[1]).toEqual([{ method: 'eth_requestAccounts' }]);
+    // eslint-disable-next-line
+    expect(ethereum.request.mock.calls).toHaveLength(2);
+
+    const TO_WALLET_ADDRESS = '0x12345';
     const AMOUNT_TEXT = '100';
 
     const sendButton = queryByText('Send');
-    const amountInput = queryByLabelText('Amount');
+    const amountInput = queryByTestId('Amount');
+    const toInput = queryByTestId('To Address');
 
+    await fireEvent.update(toInput, TO_WALLET_ADDRESS);
     await fireEvent.update(amountInput, AMOUNT_TEXT);
     await fireEvent.click(sendButton);
 
     // eslint-disable-next-line
-    // expect(ethereum.request.mock.calls[2]).toEqual([{
-    //   method: 'eth_sendTransaction',
-    //   params: [
-    //     {
-    //       from: MOCK_WALLET_ADDRESS,
-    //       to: USDC_CONTRACT_ADDRESS,
-    //       data: TO_WALLET_ADDRESS + ', ' + toHex(Number(AMOUNT_TEXT) * 1000000),
-    //       gasPrice: DEFAULT_GAS_PRICE,
-    //     },
-    //   ],
-    // }]);
+    expect(ethereum.request.mock.calls[2]).toEqual([{
+      method: 'eth_sendTransaction',
+      params: [
+        {
+          from: MOCK_WALLET_ADDRESS,
+          to: USDC_CONTRACT_ADDRESS,
+          data: TO_WALLET_ADDRESS + ', ' + toHex(Number(AMOUNT_TEXT) * 1000000),
+          gasPrice: DEFAULT_GAS_PRICE,
+        },
+      ],
+    }]);
   });
-}); 
+});
+

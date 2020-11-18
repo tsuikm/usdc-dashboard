34760c7a74a0e0d2b48c206a362defd514ab76b0
Terrance Li <terranceli.terbear@gmail.com>
2020-11-18 11:27:54 -0800

    NavBar Component Refactor (#118)
    * Refactored navbar; updated tests
    
    * Remove need for web3 mock
    
    * Shorten NavBar tests
    
    * Update search input placeholder

--- tests/unit/NavBar.spec.js
+++ tests/unit/NavBar.spec.js
@@ -2,81 +2,65 @@ import { fireEvent, render } from '@testing-library/vue';
 import NavBar from '@/components/NavBar.vue';
 import { padHex } from '@/utils/utils';
 import { WEB3_BALANCEOF_ADDRESS_LENGTH } from '@/utils/constants';
-import Web3 from 'web3';
-
-Web3.VALID_ADDRESSES = [
-  '0x36f80a0bde5020ab0880ab54',
-  '0xfa2ec023f531cf6fa04c3536',
-  '0x0bd4dcdf07629fee5d4363c7',
-  '0xe7e31d0ef8c598b13e0992e2',
-].map(address => padHex(address, WEB3_BALANCEOF_ADDRESS_LENGTH));
 
 const finishPromises = async () => new Promise(resolve => setTimeout(resolve, 0));
+
+const SAMPLE_ADDRESS = padHex('0x36f80a0bde5020ab0880ab54', WEB3_BALANCEOF_ADDRESS_LENGTH);
+const BLOCKCHAIN_PATHS = ['', '/solana', '/algorand'];
 
 describe('NavBar', () => {
   it('Search Bar Displayed Correctly', () => {
     const { getByPlaceholderText } = render(NavBar);
 
-    expect(getByPlaceholderText('Wallet Address or Txn Hash')).not.toBeNull();
+    expect(getByPlaceholderText('Search Address...')).not.toBeNull();
   });
 
   it('Search Bar Valid Address Functionality', async () => {
     const router = [];
+    const route = { path: '' };
     const { getByPlaceholderText } = render(NavBar, {
       mocks: {
         $router: router,
+        $route: route,
       },
     });
-    const input = getByPlaceholderText('Wallet Address or Txn Hash');
-    await fireEvent.update(input,  Web3.VALID_ADDRESSES[0]);
-    await finishPromises();
-    await fireEvent.keyDown(input, { key: 'Enter', code: 'Enter' });
-    await finishPromises();
 
-    const url = '/address/' + Web3.VALID_ADDRESSES[0];
-    expect(router.length).toBe(1);
-    expect(router[0].path).toEqual(url);
+    const input = getByPlaceholderText('Search Address...');
+
+    for (const path of BLOCKCHAIN_PATHS) {
+      route.path = path;
+      await fireEvent.update(input,  SAMPLE_ADDRESS);
+      await fireEvent.keyDown(input, { key: 'Enter', code: 'Enter' });
+      await finishPromises();
+
+      expect(router[router.length - 1].path).toEqual(`${path}/address/${SAMPLE_ADDRESS}`);
+    }
+
+    expect(router.length).toBe(3);
   });
 
-  it('Search Bar Invalid Address Functionality', async () => {
+  it('Displays page links correctly', async () => {
     const router = [];
-    const { getByPlaceholderText } = render(NavBar, {
+    const route = { path: '' };
+    const { getByText } = render(NavBar, {
       mocks: {
         $router: router,
+        $route: route,
       },
     });
 
-    const input = getByPlaceholderText('Wallet Address or Txn Hash');
-    await fireEvent.update(input, 'invalid-address');
-    await finishPromises();
-    await fireEvent.keyDown(input, { key: 'Enter', code: 'Enter' });
-    await finishPromises();
+    expect(getByText('Accounts')).not.toBeNull();
+    expect(getByText('Transfer')).not.toBeNull();
 
-    const url = '/404';
-    expect(router.length).toBe(1);
-    expect(router[0].path).toEqual(url);
-  });
-
-  describe('Other links', () => {
-    it('Displays other links correctly', async () => {
-      const router = [];
-      const { getByText } = render(NavBar, {
-        mocks: {
-          $router: router,
-        },
-      });
-      expect(getByText('Accounts')).not.toBeNull();
-      expect(getByText('Transfer')).not.toBeNull();
+    for (const path of BLOCKCHAIN_PATHS) {
+      route.path = path;
+      await finishPromises();
 
       await fireEvent.click(getByText('Accounts'));
-
-      expect(router.length).toBe(1);
-      expect(router[0].path).toEqual('/accounts');
-
+      expect(router[router.length - 1].path).toEqual(`${path}/accounts`);
       await fireEvent.click(getByText('Transfer'));
-
-      expect(router.length).toBe(2);
-      expect(router[1].path).toEqual('/transfers');
-    });
+      expect(router[router.length - 1].path).toEqual(`${path}/transfers`);
+    }
+    expect(router.length).toBe(6);
   });
 });

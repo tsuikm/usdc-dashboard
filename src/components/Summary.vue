2a6be1010cc54ebf5ac834fbc8b9b79ef9d68b63
Brandon Li <brandon.li820@icloud.com>
2020-11-18 23:59:41 -0700

    summary page prop def

---
+++ src/components/Summary.vue
@@ -0,0 +1,45 @@
+<template>
+
+</template>
+
+<script>
+
+export default {
+  name: 'Summary',
+  props: {
+
+    /**
+     * @param {Role[]} - the roles to display.
+     * @typedef {Role} {
+     *   name: {String} - the name of the Role (eg. 'Pauser').
+     *   addresses: {String[]} - the addresses that have this particular role.
+     * }
+     */
+    roles: {
+      type: Array,
+      validator: role => role instanceof Object &&
+                         typeof role.name === 'string' &&
+                         Array.isArray(role.addresses) &&
+                         role.addresses.every(address => typeof address === 'string' && address.length)
+    },
+
+    /**
+     * @param {String[]} - the transactions to display.
+     */
+    transactions: {
+      type: Array,
+      validator: transactions => Array.isArray(transaction) &&
+                                 transactions.every(hash => typeof hash === 'string' && address.length)
+    },
+
+    /**
+     * @param {number[]} - the blocks to display, as an array of base-10 numbers.
+     */
+    blocks: {
+      type: Array,
+      validator: blocks => Array.isArray(blokcs) && blocks.every(block => typeof block === 'number')
+    },
+
+  },
+};
+</script>

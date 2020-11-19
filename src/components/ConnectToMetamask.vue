5163d6517a0d572ba9b6356d1e069213a8b3df38
Kelly Tsai <kellytsai@berkeley.edu>
2020-11-19 15:29:01 +0800

    added connect to metamask component

---
+++ src/components/ConnectToMetamask.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class='connectButton'>
+    <md-button
+      data-testid="transfers-card-connect-button"
+      @click="connectMetamask"
+    >
+      Connect to Metamask
+    </md-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ConnectToMetamask',
+  methods: {
+    async connectMetamask() {
+      try {
+        // eslint-disable-next-line
+        this.accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.connectButton {
+  color: #1ED67D;
+}
+</style>

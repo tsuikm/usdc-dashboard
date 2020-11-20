42e1c710a0c65c697fa32a09eb9bc48e58f88c94
Brandon Li <brandon.li820@icloud.com>
2020-11-19 17:36:54 -0700

    add strict handling of unhandled promise rejection

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -5,3 +5,15 @@ import { config } from '@vue/test-utils';
 
 Vue.use(VueMaterial);
 config.stubs['nuxt-link'] = NuxtLinkStub;
+
+// Strictly handle `unhandledRejection` warnings.
+//
+// For the context of our project, we only use async/await, so these warning are
+// thrown errors in async contexts (aka an error, meaning our tests shouldn't
+// have passed).
+//
+// For now, listen to these warnings and treat these as errors.
+// Solution derived from https://github.com/facebook/jest/issues/3251#issuecomment-298831246
+process.on('unhandledRejection', reason => {
+  throw reason;
+});

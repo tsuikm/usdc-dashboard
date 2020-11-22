7327ede6b38682afd7d1cada6191df9ac38235a7
Stephanie Xiao <stephaniexiao@Stephanies-MacBook-Pro.local>
2020-11-22 01:23:49 -0800

    cherry-pick action button

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button class="button" @click="onClick">{{ label }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ActionButton',
+  props: {
+    label: String,
+    onClick: Function,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/styles/variables/_colors.scss";
+
+.button {
+  height: 50px;
+  padding-left: 40px;
+  padding-right: 40px;
+  border-radius: 5px;
+  background-color: #1ED67D;
+  border: none;
+  color: #ffffff;
+  font-weight: 900;
+  cursor: pointer;
+}
+
+</style>

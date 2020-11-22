0b3de2b6181ce4a2b46b754ded8be11c12e001f3
Kelly Tsai <kellytsai@berkeley.edu>
2020-11-22 16:37:10 +0800

    added address page component

---
+++ src/components/AddressPage.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="content">
+    <h1> Address Details </h1>
+
+    <!-- wallet address -->
+    <div class="wallet-address">
+      <h2> Wallet Address </h2>
+      <div> {{this.$route.params.address}} </div>
+    </div>
+
+    <!-- roles -->
+    <div class="roles">
+      <h2> Roles </h2>
+      <div
+        v-for="(role, roleIndex) in roles"
+        :key="roleIndex"
+        class="role"
+      >
+        <div> {{ role }} </div>
+      </div>
+    </div>
+
+    <!-- balance -->
+    <div class="balance">
+      <h2> Balance </h2>
+      <div> ${{ this.balance }} </div>
+    </div>
+
+    <!-- blacklisted -->
+    <div class="blacklisted">
+      <h2> Blacklisted? </h2>
+      <div v-if="this.isBlacklisted"> Yes </div>
+      <div v-else> No </div>
+    </div>
+
+    <!-- transactions -->
+    <div class="transactions">
+      <h2> Address Transactions </h2>
+      <div>
+        <nuxt-link
+          v-for="(transaction, index) in transactions"
+          :key="index"
+          :to="`${basePath}/transaction/${transaction}`"
+        >
+          {{ transaction }}
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { basePathFromPath } from '@/utils/utils';
+
+export default {
+  name: 'AddressPage',
+   props: {
+
+     /**
+      * @param {Role[]} - the roles to display.
+      * @typedef {Role} {
+      *   name: {String} - the name of the Role (eg. 'Pauser').
+      *   addresses: {String[]} - the addresses that have this particular role.
+      *   icon: {String} - the file of the icon displayed for the role, relative to the @/assets directory.
+      * }
+      */
+     roles: {
+       type: Array,
+       validator: roles => Array.isArray(roles) &&
+                           roles.every(role => typeof role === 'string'),
+     },
+
+     /**
+      * @param {String[]} - the transactions to display.
+      */
+     transactions: {
+       type: Array,
+       validator: transactions => Array.isArray(transactions) &&
+                                  transactions.every(hash => typeof hash === 'string' && hash.length),
+     },
+
+     /**
+      * @param {IsBlacklisted} - whether the address is blacklisted.
+      */
+     isBlacklisted: {
+       type: Boolean,
+       validator: isBlacklisted => typeof isBlacklisted === 'boolean',
+     },
+
+     /**
+      * @param {Balance} - the balance of the address.
+      */
+     balance: {
+       type: Number,
+       validator: balance => typeof balance === 'number',
+     },
+   },
+   computed: {
+     basePath() {
+       return this.$route ? basePathFromPath(this.$route.path) : '';
+     },
+   },
+}
+</script>
+
--- src/pages/address/_address.vue
+++ src/pages/address/_address.vue
@@ -1,21 +1,72 @@
 <template>
   <div>
     <NavBar />
-    <Overview :wallet-address="this.$route.params.address" />
-    <Transactions :address="this.$route.params.address" />
+    <AddressPage 
+      :roles="this.roles" 
+      :isBlacklisted="this.isBlacklisted"
+      :balance="this.balance"
+    />
   </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar';
-import Overview from '@/components/Overview';
-import Transactions from '@/components/Transactions';
+import AddressPage from '@/components/AddressPage';
+import { WEB3_BALANCEOF_ADDRESS_LENGTH } from '@/utils/constants';
+import { padHex } from '@/utils/utils';
+import { contract, web3, getBalance, getTotalSupply } from '@/utils/web3utils';
 
 export default {
   components: {
-    Overview,
-    Transactions,
     NavBar,
+    AddressPage,
+  },
+  data() {
+    return {
+      balance: null,
+      isBlacklisted: false,
+      roles: [],
+      transactions: [],
+    };
+  },
+  created: function () {
+    this.lookupBalance();
+    this.lookupBlacklisted();
+    this.checkRoles();
+  },
+  methods: {
+    async lookupBalance() {
+      if (this.$route.params.address === '') {
+        return;
+      }
+
+      this.balance = await getBalance(this.$route.params.address);
+    },
+    async lookupBlacklisted() {
+      if (this.$route.params.address === '') {
+        return;
+      }
+
+      this.isBlacklisted = await contract.methods
+        .isBlacklisted(
+          padHex(this.$route.params.address, WEB3_BALANCEOF_ADDRESS_LENGTH),
+        )
+        .call();
+    },
+    async checkRoles() {
+      if (await contract.methods.isMinter(this.$route.params.address).call()) {
+        this.roles.push('minter');
+      };
+      const pauserAddress = await contract.methods.pauser().call();
+      if (pauserAddress === this.$route.params.address) {
+        this.roles.push('pauser');
+      };
+      const owner = await contract.methods.owner().call();
+      const ownerAddress = padHex(owner, WEB3_BALANCEOF_ADDRESS_LENGTH);
+      if (ownerAddress === this.$route.params.address) {
+        this.roles.push('owner');
+      };
+    },
   },
 };
 </script>

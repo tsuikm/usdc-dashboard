801feebfd65fe5e3479f30cef69db32dac55de08
Brandon Li <brandon.li820@icloud.com>
2020-11-21 22:39:21 -0700

    summary component refactor (#121)
    * summary page prop def
    
    * dom
    
    * update pages/index.vue to use Summary
    
    * fixes
    
    * move styles
    
    * some small styling changes
    
    * remove blacklister from api
    
    * finish tests for summary component
    
    * simplify and fix tests for ethereum summary page
    
    * lint
    
    * lint
    
    * fix tests
    
    * styling
    
    * icons
    
    * lint
    
    * fix tests
    
    * Responsive
    
    * lint
    
    * lint
    
    * switch ordering of roles
    
    * margins

--- src/api/rest.js
+++ src/api/rest.js
@@ -21,19 +21,4 @@ app.get('/minters', async (req, res) => {
   }
 });
 
-app.get('/blacklister', async (req, res) => {
-  const sqlQuery = 'SELECT block_number, newBlacklister FROM `blockchain-etl.ethereum_usdc.FiatTokenV1_event_BlacklisterChanged` ORDER BY block_number DESC LIMIT 1';
-  const options = {
-    query: sqlQuery,
-    location: 'US',
-  };
-
-  try {
-    const [rows] = await bigqueryClient.query(options);
-    res.json(rows[0].newBlacklister);
-  } catch (e) {
-    res.status(500).end();
-  }
-});
-
 module.exports = app;

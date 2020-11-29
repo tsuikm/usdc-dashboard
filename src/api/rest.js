3ff395999f0985f7cfd6c010bdca9c4cad4cbed6
Brandon Li <brandon.li820@icloud.com>
2020-11-29 01:22:11 -0700

    todo comment

--- src/api/rest.js
+++ src/api/rest.js
@@ -1,7 +1,6 @@
 const bodyParser = require('body-parser');
 const app = require('express')();
 const { BigQuery } = require('@google-cloud/bigquery');
-const algosdk = require('algosdk');
 
 const bigqueryClient = new BigQuery();
 
@@ -22,23 +21,29 @@ app.get('/minters', async (req, res) => {
   }
 });
 
-const baseServer = 'https://mainnet-algorand.api.purestake.io/idx2/';
-const port = '';
-const token = {
-  'X-API-Key': 'Yux8PVogsu1Z7tFByiIvh7npNLq9daJsp0pDmaj6',
-};
-const client = new algosdk.Indexer(token, baseServer, port);
 
-app.get('/test', async (req, res) => {
+// TODO: initial approach was to create our own API endpoints. We tentatively bailed on this approach
+//       to directly use purestake's rest endpoints. We will investigate if this resolves issue where part
+//       of the response string is cut off, resulting in a JSON parsing error.
+//
+// // const algosdk = require('algosdk');
+// const baseServer = 'https://mainnet-algorand.api.purestake.io/idx2/';
+// const port = '';
+// const token = {
+//   'X-API-Key': 'Yux8PVogsu1Z7tFByiIvh7npNLq9daJsp0pDmaj6',
+// };
+// const client = new algosdk.Indexer(token, baseServer, port);
 
-  try {
-    const txns = await client.searchForTransactions().do();
-    res.json(txns);
-  } catch (e) {
-    console.log(e);
-    res.status(500).end();
-  }
-});
+// app.get('/test', async (req, res) => {
+
+//   try {
+//     const txns = await client.searchForTransactions().do();
+//     res.json(txns);
+//   } catch (e) {
+//     console.log(e);
+//     res.status(500).end();
+//   }
+// });
 
 
 module.exports = app;

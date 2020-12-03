6912a27cb19b2feb6ea266a33a27f083a982822a
youzhentsai <52082035+youzhentsai@users.noreply.github.com>
2020-12-03 14:42:34 +0800

    Address page component refactor (#140)
    * added address page component
    
    * removed unused imports
    
    * Adding style for transactions
    
    * Added grid layout
    
    * Fixed table and added more styling for role chips
    
    * added test for address page component
    
    * More styling
    
    * test for address details page
    
    * fixed mock problems
    
    * added page in params for test
    
    * debugging test
    
    * fixed test and lint
    
    * delete commented code and fixed comment
    
    * refactored styling for chip
    
    * deleted unnecessary components and fixed test
    
    * fix lint errors
    
    Co-authored-by: unknown <tetsuroescudero@berkeley.edu>

--- src/pages/address/_address.vue
+++ src/pages/address/_address.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <NavBar />
-    <Overview :wallet-address="this.$route.params.address" />
+    <AddressPage 
+      :roles="this.roles" 
+      :is-blacklisted="this.isBlacklisted"
+      :balance="this.balance"
+    />
+    <h1> Address Transactions </h1>
     <Table
       ref="table"
       :loading="loading"
@@ -17,20 +22,24 @@
 
 <script>
 import NavBar from '@/components/NavBar';
-import Overview from '@/components/Overview';
-import Table from '@/components/Table';
-import { TRANSACTION_SCHEMA, WEB3_GET_LOGS_ADDRESS_LENGTH } from '@/utils/constants';
+import AddressPage from '@/components/AddressPage';
 import { padHex } from '@/utils/utils';
+import { contract, getBalance } from '@/utils/web3utils';
 import { getWalletTransactions, fetchAge } from '@/utils/web3utils';
+import { TRANSACTION_SCHEMA, WEB3_GET_LOGS_ADDRESS_LENGTH } from '@/utils/constants';
+import Table from '@/components/Table';
 
 export default {
   components: {
-    Overview,
-    Table,
     NavBar,
+    AddressPage,
+    Table,
   },
   data() {
     return {
+      balance: null,
+      isBlacklisted: false,
+      roles: [],
       transactions: [],
       address: null,
       loading: true,
@@ -41,12 +50,61 @@ export default {
       return TRANSACTION_SCHEMA;
     },
   },
+  created: function () {
+    this.lookupBalance();
+    this.lookupBlacklisted();
+    this.checkRoles();
+  },
   async mounted() {
     await this.fetchTransactions();
     await this.fetchAges(0);
     this.loading = false;
   },
   methods: {
+    async lookupBalance() {
+      if (this.$route.params.address === '') {
+        return;
+      }
+
+      this.balance = await getBalance(this.$route.params.address);
+    },
+    async lookupBlacklisted() {
+      if (this.$route.params.address === '') {
+        return;
+      }
+
+      this.isBlacklisted = await contract.methods
+        .isBlacklisted(this.$route.params.address).call();
+    },
+    async checkRoles() {
+      if (await contract.methods.isMinter(this.$route.params.address).call()) {
+        this.roles.push({
+          name: 'Minter',
+          color: '#4FE39C',
+        });
+      }
+      const pauserAddress = await contract.methods.pauser().call();
+      if (pauserAddress === this.$route.params.address) {
+        this.roles.push({
+          name: 'Pauser',
+          color: '#1AA3FF',
+        });
+      }
+      const owner = await contract.methods.owner().call();
+      if (owner === this.$route.params.address) {
+        this.roles.push({
+          name: 'Owner',
+          color: '#9F72FF',
+        });
+      }
+      const blacklisterAddress = await contract.methods.blacklister().call();
+      if (blacklisterAddress === this.$route.params.address) {
+        this.roles.push({
+          name: 'Blacklister',
+          color: '#4FE39C',
+        });
+      }
+    },
     async fetchTransactions() {
       this.transactions = await getWalletTransactions(padHex(this.$route.params.address, WEB3_GET_LOGS_ADDRESS_LENGTH));
     },
@@ -54,13 +112,10 @@ export default {
       const pageLength = this.$refs.table.pageLength;
       const upperBound = Math.min((page + 1) * pageLength, this.transactions.length);
       const promises = [];
-
       for (let i = page * pageLength; i < upperBound; i++) {
         promises.push(fetchAge(this.transactions[i]));
       }
-
       const ages = await Promise.all(promises);
-
       for (let i = page * pageLength; i < upperBound; i++) {
         this.transactions[i].age = ages[i - page * pageLength];
       }
@@ -69,3 +124,7 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+
+</style>

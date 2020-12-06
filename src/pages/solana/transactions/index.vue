c17ab52209c66926be39208d840fb268368e9171
Terrance Li <terranceli.terbear@gmail.com>
2020-12-05 21:03:01 -0800

    Basic transactions page

---
+++ src/pages/solana/transactions/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <NavBar />
+    <h1>Solana USDC Transactions</h1>
+    <Table
+      ref="table"
+      :loading="loading"
+      name=""
+      :total-items="transactions.length"
+      :schema="this.tableSchema"
+      :content="this.transactions"
+      :key-field="'Transaction Hash'"
+      @page:change="this.pageChange"
+    />
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar';
+import Table from '@/components/Table';
+import { getRecentTransactions, SOLANA_TRANSACTION_SCHEMA } from '@/utils/solana';
+// import { fetchAge, getAllTransactions } from '@/utils/web3utils';
+
+export default {
+  components: {
+    Table,
+    NavBar,
+  },
+  data() {
+    return {
+      transactions: [],
+      loading: true,
+    };
+  },
+  computed: {
+    tableSchema() {
+      return SOLANA_TRANSACTION_SCHEMA;
+    },
+  },
+  async mounted() {
+    await this.fetchTransactions();
+    // await this.fetchAges(this.$refs.table.page);
+    this.loading = false;
+  },
+  methods: {
+    async fetchTransactions() {
+      this.transactions = (await getRecentTransactions(1000)).result;
+      console.log(this.transactions);
+    },
+    // async fetchAges(page) {
+    //   const pageLength = this.$refs.table.pageLength;
+    //   const upperBound = Math.min((page + 1) * pageLength, this.transactions.length);
+    //   const promises = [];
+
+    //   for (let i = page * pageLength; i < upperBound; i++) {
+    //     promises.push(fetchAge(this.transactions[i]));
+    //   }
+
+    //   const ages = await Promise.all(promises);
+
+    //   for (let i = page * pageLength; i < upperBound; i++) {
+    //     this.transactions[i].age = ages[i - page * pageLength];
+    //   }
+    // },
+    async pageChange(page) {
+      this.loading = true;
+      await this.fetchAges(page);
+      this.loading = false;
+    },
+  },
+  head() {
+    return {
+      title: 'Transactions',
+    };
+  },
+};
+</script>

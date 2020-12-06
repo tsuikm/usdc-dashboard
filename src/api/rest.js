997b512260d3a92811c636a26915fce5507ab813
Brandon Li <brandon.li820@icloud.com>
2020-12-06 14:35:31 -0700

    move algorand-rest stuff to separate file

--- src/api/rest.js
+++ src/api/rest.js
@@ -21,51 +21,6 @@ app.get('/minters', async (req, res) => {
   }
 });
 
-
-// TODO: initial approach was to create our own API endpoints. We tentatively bailed on this approach
-//       to directly use purestake's rest endpoints. We will investigate if this resolves issue where part
-//       of the response string is cut off, resulting in a JSON parsing error.
-//
-const algosdk = require('algosdk');
-const baseServer = 'https://mainnet-algorand.api.purestake.io';
-const port = '';
-const token = {
-  'X-API-Key': process.env.PURESTAKE_API_KEY,
-};
-
-const indexer = new algosdk.Indexer(token, `${baseServer}/idx2/`, port);
-
-app.get('/algorand', async (req, res) => {
-  while (true) {
-    try {
-      let response;
-      if (req.query.request === 'transactions') {
-        response = indexer.searchForTransactions(req.query.param);
-      }
-      if (req.query.request === 'blocks') {
-        response = indexer.lookupBlock(req.query.param);
-      }
-      if (req.query.request === 'assets') {
-        response = indexer.lookupAssetByID(req.query.param);
-      }
-      if (req.query.request === 'accounts') {
-        response = indexer.searchAccounts(req.query.param);
-      }
-      if (req.query.request === 'account') {
-        response = indexer.lookupAccountByID(req.query.param);
-      }
-      response.query = req.query;
-
-      return res.json(await response.do());
-    }
-    catch (e) {
-      if (e.message === 'Too Many Requests') {
-        await new Promise(r => setTimeout(r, 2000));
-        continue;
-      }
-      return res.status(500).end();
-    }
-  }
-});
+app.get('/algorand', require('./algorand-rest').get);
 
 module.exports = app;

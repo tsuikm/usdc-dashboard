3326cc74cf86d3a6ce08cea1c53811fb4ed485fa
Terrance Li <terranceli.terbear@gmail.com>
2020-12-07 22:32:51 -0800

    Added comment on temporary solution

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -78,6 +78,9 @@ export default {
         role.addresses.push(address);
       }
 
+      // This is for hiding Minters section when not on Mainnet (since minters are fetch via 
+      // BigQuery rather than web3). This is a temporary solution, as there does not yet exist 
+      // a USDC BigQuery for Ropsten.
       if (addresses.length === 0) {
         this.roles = this.roles.filter(role => role.name !== roleName);
       }
